91273734ea2597a94b67aef7ac3472fa5c17c3d5
Lucy Kushinada <lucykushinada00@gmail.com>
2024-05-04 23:43:48 +0530

    Added screenshots

---
+++ src/components/Screenshots.js
@@ -0,0 +1,10 @@
+import clsx from 'clsx';
+import styles from './Screenshots.module.css';
+
+export default function Screenshots(props) {
+    return (
+        <div className={clsx(styles.screenshotsContainer)}>
+            {props.children}
+        </div>
+    );
+}
