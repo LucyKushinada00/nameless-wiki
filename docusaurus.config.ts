8f95f0b6cb4b351fe5aac77eb5f3d1faa5f2ab14
Lucy Kushinada <LucyKushinada00@gmail.com>
2025-12-07 14:33:49 +0530

    Updated Docusaurus and removed node version constraint

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,46 +1,51 @@
-import type * as Preset from '@docusaurus/preset-classic';
-import type { Config } from '@docusaurus/types';
-import 'dotenv/config';
-import { themes as prismThemes } from 'prism-react-renderer';
+import type * as Preset from "@docusaurus/preset-classic";
+import type { Config } from "@docusaurus/types";
+import "dotenv/config";
+import { themes as prismThemes } from "prism-react-renderer";
 
 const config: Config = {
-  title: 'Nameless CLO',
-  tagline: 'Nameless CLO is based on CodeLinaro and inspired by Qualcomm. We offer a smooth, stable, and high-performance experience for your Snapdragon device.',
-  favicon: 'img/favicon.ico',
+  title: "Nameless CLO",
+  tagline:
+    "Nameless CLO is based on CodeLinaro and inspired by Qualcomm. We offer a smooth, stable, and high-performance experience for your Snapdragon device.",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://nameless-wiki.vercel.app',
+  url: "https://nameless-wiki.vercel.app",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'LucyKushinada00', // Usually your GitHub org/user name.
-  projectName: 'nameless-wiki', // Usually your repo name.
+  organizationName: "LucyKushinada00", // Usually your GitHub org/user name.
+  projectName: "nameless-wiki", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: "warn",
+    },
+  },
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          routeBasePath: '/',
-          sidebarPath: './sidebars.ts',
+          routeBasePath: "/",
+          sidebarPath: "./sidebars.ts",
         },
         blog: false,
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -49,32 +54,37 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     announcementBar: {
-      content: '✨ Nameless is now based on CLO starting from Android 15. New builds for supported devices are coming soon. Stay tuned. <a target="_blank" rel="noopener noreferrer" href="https://t.me/nameless_ophub/599814">Know More</a> ✨',
+      content:
+        '✨ Nameless is now based on CLO starting from Android 15. New builds for supported devices are coming soon. Stay tuned. <a target="_blank" rel="noopener noreferrer" href="https://t.me/nameless_ophub/599814">Know More</a> ✨',
       isCloseable: false,
     },
     footer: {
-      copyright: 'Created with ❤️ by Lucy Kushinada and the Nameless Team',
+      copyright: "Created with ❤️ by Lucy Kushinada and the Nameless Team",
     },
     algolia: {
       appId: process.env.ALGOLIA_APP_ID,
       apiKey: process.env.ALGOLIA_API_KEY,
-      indexName: 'nameless-wiki',
+      indexName: "nameless-wiki",
       contextualSearch: false,
     },
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/docusaurus-social-card.jpg",
     colorMode: {
       respectPrefersColorScheme: true,
     },
     navbar: {
-      title: 'Nameless CLO',
+      title: "Nameless CLO",
       items: [
-        { to: '/screenshots', label: 'Screenshots', position: 'left' },
-        { to: '/faq', label: 'FAQ', position: 'left' },
-        { to: '/category/flash-instructions', label: 'Flash Instructions', position: 'left' },
+        { to: "/screenshots", label: "Screenshots", position: "left" },
+        { to: "/faq", label: "FAQ", position: "left" },
         {
-          href: 'https://github.com/Nameless-AOSP',
-          label: 'GitHub',
-          position: 'right',
+          to: "/category/flash-instructions",
+          label: "Flash Instructions",
+          position: "left",
+        },
+        {
+          href: "https://github.com/Nameless-AOSP",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
